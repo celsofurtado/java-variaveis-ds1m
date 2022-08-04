56b3b64bc7927b63f35447ef7e7d4cfe0b5f430b
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2022-08-04 11:22:19 -0300

    Versão 1

---
+++ src/br/senai/sp/jandira/JurosSimples.java
@@ -0,0 +1,32 @@
+package br.senai.sp.jandira;
+
+import java.util.Scanner;
+
+public class JurosSimples {
+
+	public static void main(String[] args) {
+		
+		double juros, capital, taxa, tempo;
+		Scanner leitor;
+		
+		leitor = new Scanner(System.in);
+		
+		System.out.print("Qual é o capital? ");
+		capital = leitor.nextDouble();
+		
+		System.out.print("Qual é a taxa de juros? ");
+		taxa = leitor.nextDouble();
+		
+		System.out.println("Qual o tempo? ");
+		tempo = leitor.nextDouble();
+		
+		juros = capital * taxa / 100 * tempo; // J = C.i.t
+		
+		System.out.println("O valor do juros é: " + juros);
+		System.out.printf("O valor do juros é %s", juros);
+		System.out.println();
+		System.out.printf("Ao final do período você terá R$%s", juros + capital);
+
+	}
+
+}
